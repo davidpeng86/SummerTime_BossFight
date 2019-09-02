3c4d015a7c07f2bc7bc0792da6e2b935eee4735f
Dubito-mac <peng86david@gmail.com>
2019-09-02 17:22:30 +0800

    can shoot

---
+++ Assets/Scripts/BulletMotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMotion : MonoBehaviour
+{
+    Rigidbody rb;
+    public int force = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * force, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision other) {
+        print("Collision");
+        ContactPoint contactPoint = other.GetContact(0);
+        Vector3 curDir = Vector3.forward;
+        Vector3 newDir = Vector3.Reflect(curDir, contactPoint.normal);
+        // transform.rotation = Quaternion.FromToRotation(curDir, newDir);
+        // print(rb.GetPointVelocity(transform.position));
+        rb.AddForce(contactPoint.normal * force/2, ForceMode.Impulse);
+    }
+}
